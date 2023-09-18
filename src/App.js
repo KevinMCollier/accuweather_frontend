813b911d8d6bf0453031b70b6e9910203dc05411
kevin collier <collier.kevin.m@gmail.com>
2023-09-18 10:31:58 +0900

    removed initial default style and added new css files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
+import './assets/styles/global.css'
 import React from 'react';
 import WeatherDashboard from './components/WeatherDashboard';
 
@@ -7,19 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <h1>Kevin's Sunny Forecast 🌤</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
       <main>
         <WeatherDashboard />
