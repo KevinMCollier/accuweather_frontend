65df6bd9e41f055705952133ce15e862d59aa49d
kevin collier <collier.kevin.m@gmail.com>
2023-09-18 10:33:52 +0900

    added more details to weather display

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -8,18 +8,21 @@ function WeatherDisplay({ weather }) {
   const feelsLike = weather?.weather?.feels_like;
   const weatherIcon = weather?.weather?.icon;
   const weatherDescription = weather?.weather?.description;
+  const minTemperature = weather?.weather?.temp_min;
+  const maxTemperature = weather?.weather?.temp_max;
+  const humidity = weather?.weather?.humidity;
+
 
   return (
     <div>
       <h2>{weather.name}</h2>
-      {temperature && <p>Temperature: {temperature} degrees</p>}
-      {feelsLike && <p>Feels like: {feelsLike} degrees</p>}
-      {weatherIcon && weatherDescription && (
-        <img
-          src={`http://openweathermap.org/img/w/${weatherIcon}.png`}
-          alt={weatherDescription}
-        />
-      )}
+      {temperature && <p>Temperature: {temperature}°C</p>}
+      {feelsLike && <p>Feels like: {feelsLike}°C</p>}
+      {minTemperature && <p>Min Temperature: {minTemperature}°C</p>}
+      {maxTemperature && <p>Max Temperature: {maxTemperature}°C</p>}
+      {humidity && <p>Humidity: {humidity}%</p>}
+      {weatherIcon && <img src={`http://openweathermap.org/img/w/${weatherIcon}.png`} alt={weatherDescription} />}
+      {weatherDescription && <p>{weatherDescription}</p>}
     </div>
   );
 }
