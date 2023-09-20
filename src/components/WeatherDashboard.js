6ba1718373b73b1ad3038704c3a3a8e76476f3b0
kevin collier <collier.kevin.m@gmail.com>
2023-09-20 22:02:21 +0900

    updated app to work only with search

--- src/components/WeatherDashboard.js
+++ src/components/WeatherDashboard.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import LocationsList from './LocationsList';
+// import LocationsList from './LocationsList';
 import WeatherData from './WeatherData';
 import Navbar from './Navbar';  // Make sure to import Navbar
 
 function WeatherDashboard() {
-  const [selectedLocationId, setSelectedLocationId] = useState(null);
+  const [selectedLocationId] = useState(null);
   const [weather, setWeather] = useState(null);
   const [loading, setLoading] = useState(true);
   const [searchedCityName, setSearchedCityName] = useState("");
+  const DEFAULT_LOCATION = 'Tokyo';
   const handleSearchResult = (data) => {
     setWeather(data);
     setSearchedCityName(data.city_name);
@@ -15,9 +16,25 @@ function WeatherDashboard() {
   }
 
   useEffect(() => {
-    if (!selectedLocationId) return;  // Only fetch if a location is selected
-
-    fetch(`http://localhost:3000/api/v1/locations/${selectedLocationId}`)
+    if (selectedLocationId) {
+      fetch(`http://localhost:3000/api/v1/locations/${selectedLocationId}`)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json();
+        })
+        .then(data => {
+          setWeather(data);
+          setLoading(false);
+        })
+        .catch(error => {
+          console.error("There was an error fetching the weather data", error);
+          setLoading(false);
+        });
+    } else {
+            // Default fetch when the component mounts for the DEFAULT_LOCATION
+      fetch(`http://localhost:3000/api/v1/locations/search?query=${DEFAULT_LOCATION}`)
       .then(response => {
         if (!response.ok) {
           throw new Error('Network response was not ok');
@@ -26,20 +43,22 @@ function WeatherDashboard() {
       })
       .then(data => {
         setWeather(data);
+        setSearchedCityName(data.city_name);
         setLoading(false);
       })
       .catch(error => {
         console.error("There was an error fetching the weather data", error);
         setLoading(false);
       });
+    }
   }, [selectedLocationId]);
 
   return (
     <div>
       <Navbar weather={weather} loading={loading} onSearch={handleSearchResult} />
       <h3>Weather for {searchedCityName}</h3>
-      <LocationsList setSelectedLocationId={setSelectedLocationId} />
-      {selectedLocationId && <WeatherData weather={weather} loading={loading} />}
+      {/* <LocationsList setSelectedLocationId={setSelectedLocationId} /> */}
+      {weather && <WeatherData weather={weather} loading={loading} />}
     </div>
   );
 }
