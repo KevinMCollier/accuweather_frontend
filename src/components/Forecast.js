87eb4db736fd6895a9904c7c910f59dcc9059e62
kevin collier <collier.kevin.m@gmail.com>
2023-09-22 19:06:55 +0900

    added forecast file and container

---
+++ src/components/Forecast.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Forecast.css';
+
+function Forecast({ forecastData }) {
+  if (!forecastData) return null;
+
+  return (
+    <div className="forecast-card">
+      {forecastData.map((data, index) => {
+        const temperature = data?.weather?.temperature;
+        const feelsLike = data?.weather?.feels_like;
+        const weatherIcon = data?.weather?.icon;
+        const weatherDescription = data?.weather?.description;
+
+        return (
+          <div key={index} className="forecast-item">
+            <div className="forecast-icon">
+              {weatherIcon && <img src={`http://openweathermap.org/img/w/${weatherIcon}.png`} alt={weatherDescription} />}
+            </div>
+            <div className="forecast-temperature">{temperature}°</div>
+            <div className="forecast-feels-like">Feels like {feelsLike}°</div>
+            <div className="forecast-description">{weatherDescription}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default Forecast;
---
+++ src/components/ForecastContainer.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import Forecast from './Forecast';
+
+function ForecastContainer() {
+  const [forecastData, setForecastData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api/v1/forecast')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok')
+        }
+        return response.json();
+      })
+      .then(data => {
+        setForecastData(data);
+        setLoading(false);
+      })
+      .catch(error => {
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Erorr: {error.message}</div>;
+
+  return <Forecast forecastData={forecastData} />;
+}
+
+export default ForecastContainer;
